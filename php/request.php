c4717f8edde90cf246283337e5c12bb0c6782e50
Matthew Wadsworth <mdwadsworth@mail.fhsu.edu>
2021-02-16 15:24:16 -0600

    Just added comments to index.html.
    Added php folder & request.php. This php script will accept Get on our own
    server and verify the domain requesting the search. Then call API and relay
    to client.

---
+++ php/request.php
@@ -0,0 +1,18 @@
+<?
+/*
+script will only allow get from suggested domain and then request JSON from spoonacular and relay back to client.
+*/
+header("Content-Type: application/json");
+error_reporting(0); //need no post of warnings or errors as it could changes the json output
+
+$allowed = array('hbprophecy.com', '192.168.0.2', '127.0.0.1'); //allowed domains
+$domainname = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
+if(!in_array($domainname, $allowed)){
+	echo json_encode(array("results" => 'false'));
+	exit();
+}
+if($_GET){
+	$json = file_get_contents("https://api.spoonacular.com/recipes/search?apiKey=fbd4007d4eae44aebd9d387fc1a9292c&query=soup");
+	echo $json;
+}
+?>
