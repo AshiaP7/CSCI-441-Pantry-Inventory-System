ce1bd64b585a120897886e1a7fd2063f382f313a
Matthew Wadsworth <mdwadsworth@mail.fhsu.edu>
2021-02-18 15:34:44 -0600

    Turned the sendmail into a function so it can be utilized in other scripts.

--- php/sendmail.php
+++ php/sendmail.php
@@ -16,71 +16,74 @@ require './PHPMailer/Exception.php';
 require './PHPMailer/PHPMailer.php';
 require './PHPMailer/SMTP.php';
 
-//Create a new PHPMailer instance
-$mail = new PHPMailer();
 
-//Tell PHPMailer to use SMTP
-$mail->isSMTP();
+function SendEmail($content) {
+	//Create a new PHPMailer instance
+	$mail = new PHPMailer();
 
-//Enable SMTP debugging
-// SMTP::DEBUG_OFF = off (for production use)
-// SMTP::DEBUG_CLIENT = client messages
-// SMTP::DEBUG_SERVER = client and server messages
-//$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+	//Tell PHPMailer to use SMTP
+	$mail->isSMTP();
 
-//Set the hostname of the mail server
-$mail->Host = 'smtp.gmail.com';
-// use
-// $mail->Host = gethostbyname('smtp.gmail.com');
-// if your network does not support SMTP over IPv6
+	//Enable SMTP debugging
+	// SMTP::DEBUG_OFF = off (for production use)
+	// SMTP::DEBUG_CLIENT = client messages
+	// SMTP::DEBUG_SERVER = client and server messages
+	//$mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
-//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-$mail->Port = 587;
+	//Set the hostname of the mail server
+	$mail->Host = 'smtp.gmail.com';
+	// use
+	// $mail->Host = gethostbyname('smtp.gmail.com');
+	// if your network does not support SMTP over IPv6
 
-//Set the encryption mechanism to use - STARTTLS or SMTPS
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+	//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+	$mail->Port = 587;
 
-//Whether to use SMTP authentication
-$mail->SMTPAuth = true;
+	//Set the encryption mechanism to use - STARTTLS or SMTPS
+	$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 
-//Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = 'username@gmail.com';
+	//Whether to use SMTP authentication
+	$mail->SMTPAuth = true;
 
-//Password to use for SMTP authentication
-$mail->Password = 'password';
+	//Username to use for SMTP authentication - use full email address for gmail
+	$mail->Username = 'username@gmail.com';
 
-//Set who the message is to be sent from
-$mail->setFrom('hbprophecy@gmail.com', 'First Last');
+	//Password to use for SMTP authentication
+	$mail->Password = 'password';
 
-//Set an alternative reply-to address
-$mail->addReplyTo('replyto@example.com', 'First Last');
+	//Set who the message is to be sent from
+	$mail->setFrom('hbprophecy@gmail.com', 'First Last');
 
-//Set who the message is to be sent to
-$mail->addAddress('whoto@example.com', 'John Doe');
+	//Set an alternative reply-to address
+	$mail->addReplyTo('replyto@example.com', 'First Last');
 
-//Set the subject line
-$mail->Subject = 'PHPMailer GMail SMTP test';
+	//Set who the message is to be sent to
+	$mail->addAddress('whoto@example.com', 'John Doe');
 
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-$mail->msgHTML("Please confirm your email address by clicking the link: <a href='http://hbprophecy.com/school/php/mailconfirm.php?key=123'>http://hbprophecy.com/school/php/mailconfirm.php?key=123</a>");
+	//Set the subject line
+	$mail->Subject = 'PHPMailer GMail SMTP test';
 
-//Replace the plain text body with one created manually
-$mail->AltBody = 'This is a plain-text message body';
+	//Read an HTML message body from an external file, convert referenced images to embedded,
+	//convert HTML into a basic plain-text alternative body
+	$mail->msgHTML("Please confirm your email address by clicking the link: <a href='http://hbprophecy.com/school/php/mailconfirm.php?key=123'>http://hbprophecy.com/school/php/mailconfirm.php?key=123</a>");
 
-//Attach an image file
-//$mail->addAttachment('images/phpmailer_mini.png');
+	//Replace the plain text body with one created manually
+	$mail->AltBody = 'This is a plain-text message body';
 
-//send the message, check for errors
-if (!$mail->send()) {
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message sent!';
-    //Section 2: IMAP
-    //Uncomment these to save your message in the 'Sent Mail' folder.
-    #if (save_mail($mail)) {
-    #    echo "Message saved!";
-    #}
+	//Attach an image file
+	//$mail->addAttachment('images/phpmailer_mini.png');
+
+	//send the message, check for errors
+	if (!$mail->send()) {
+		echo 'Mailer Error: ' . $mail->ErrorInfo;
+	} else {
+		echo 'Message sent!';
+		//Section 2: IMAP
+		//Uncomment these to save your message in the 'Sent Mail' folder.
+		#if (save_mail($mail)) {
+		#    echo "Message saved!";
+		#}
+	}
 }
 
 ?>
