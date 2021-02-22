4b3dd1435f02b9b033bf01a33456732f4381872f
Matthew Wadsworth <mdwadsworth@mail.fhsu.edu>
2021-02-22 12:19:38 -0600

    Added ZXing API for barcode scanning. added properties to allow camera + flash.

--- AndroidApp/Pantry Inventory/Pantry Inventory/MainActivity.cs
+++ AndroidApp/Pantry Inventory/Pantry Inventory/MainActivity.cs
@@ -7,12 +7,15 @@ using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
 using Android.Webkit;
+using ZXing;
+using ZXing.Mobile;
 
 namespace Pantry_Inventory
 {
     [Activity(Label = "Pantry Inventory", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        MobileBarcodeScanner scanPage;
         WebView webView;
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -21,7 +24,10 @@ namespace Pantry_Inventory
             SetContentView(Resource.Layout.activity_main);
             webView = FindViewById<WebView>(Resource.Id.webView);
             webView.Settings.JavaScriptEnabled = true;
-            webView.LoadUrl("http://192.168.0.2/school/");
+            webView.LoadUrl("http://hbprophecy.com/school/");
+
+            Xamarin.Essentials.Platform.Init(Application);
+            ZXing.Net.Mobile.Forms.Android.Platform.Init();
 
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
@@ -38,6 +44,22 @@ namespace Pantry_Inventory
             int id = item.ItemId;
             if (id == Resource.Id.action_settings)
             {
+                return true;
+            }
+            if (id == Resource.Id.action_BarCode)
+            {
+                scanPage = new MobileBarcodeScanner();
+                var result = scanPage.Scan();
+                    //do something
+                    Android.App.AlertDialog.Builder alertDialog = new Android.App.AlertDialog.Builder(this);
+                    alertDialog.SetTitle("Simple Alert Dialog");
+                    alertDialog.SetMessage("BarCode: " + result.Result.Text);
+                    alertDialog.SetNeutralButton("OK", delegate
+                    {
+                        alertDialog.Dispose();
+                    });
+                    alertDialog.Show();
+                
                 return true;
             }
 
