abef5221fb61f48977ea5c6fc54fa9568161b31a
Matthew Wadsworth <mdwadsworth@mail.fhsu.edu>
2021-03-12 22:24:01 -0600

    change user.php to useraccount.php and fix up class and syntax errors.

---
+++ php/useraccount.php
@@ -0,0 +1,17 @@
+<?php
+class useraccount {
+	private $password;
+	private $email;
+	
+	public function __construct($user = null, $pw = null) {
+		//if user and pw  not null get from session vars
+		//new signin
+	}
+	public function createAccount() {
+		
+	}
+	public function updateaccount() {
+		
+	}
+}
+?>
