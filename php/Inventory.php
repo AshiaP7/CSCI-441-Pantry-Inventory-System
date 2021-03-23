7deb84bbf91ded00ed9486b142bf94cee98be06b
Matthew Wadsworth <mdwadsworth@mail.fhsu.edu>
2021-03-23 15:03:48 -0500

    Added inventory Add to inventory class. right now it only insert into table if it does not exist in our item db -> Note this needs updated since it shouuld update if exist in pantryinventory or insert into pantryinventory if exist in item table.

--- php/Inventory.php
+++ php/Inventory.php
@@ -1,15 +1,58 @@
 <?php
+include "useraccount.php";
 
-class cInventory {
-		var BarCode;
-		var Name;
+class cInventory extends useraccount {
+		private $BarCode;
+		private $Name;
 		
+		public function __construct() {
+			return parent::__construct();
+		}
 		
-		function AddToInventory {
+		public function AddToInventory($upc, $name, $image, $accountid) {
+			$mysqli = mysqli_connect(mysqlip, mysqluser, mysqlpass, "school");
+			if($mysqli->connect_errno) {
+				//echo "There was a problem connecting to server. Contact Admin.";
+				return false;
+			}
+			$query = "SELECT id, upc FROM items WHERE upc = '$upc' LIMIT 1;";
+			$result=$mysqli->query($query);
+			if($result == true && $result->num_rows > 0) {
+				//echo "Test) duplicated entry";
+				//if already exist in item dB we add it with the item id here and run query to inert ignore into pantryinventory
+				//we do ignore because itemid is unuqe and if duplicate exist it will throw and error and we ignore the insert
+				//we also need in the query to On DUPLICATE UPDATE -> To update the quantity
+				//if all querys process well here we will return true rather then false
+				$mysqli->close();
+				return false;
+			}
+			
+			/*This query only runs if Item does not exist in items table or pantryinventory -> note it shouldnt be-able to exist
+				in pantryinventory if it does not in items table because of the FK restriction CASCAD to itemid on table pantryinventory.
+			*/
+			$query = "INSERT INTO items (upc, name, image, accountid)  VALUES ('$upc', '$name', '$image', '$accountid');
+			INSERT INTO pantryinventory (accountid, itemid, quantity) VALUES('6', LAST_INSERT_ID(), '0');";
+			$mysqli->multi_query($query);
+			do {
+				if($result = $mysqli->store_result()) {
+					if($result == false) {
+						//echo "There was a problem. please contact admin for help. Statment #2 $query";
+						$result->free();
+						$mysqli->close();
+						return false;
+					}
+
+				}
+			} while($mysqli->next_result());
+			$mysqli->close();
+			return true;
+		}
+		
+		public function RemoveFromInventory() {
 			
 		}
 		
-		function RemoveFromInventory {
+		public function GetInventory() {
 			
 		}
 }
--- php/request.php
+++ php/request.php
@@ -2,22 +2,36 @@
 /*
 script will only allow get from suggested domain and then request JSON from spoonacular and relay back to client.
 */
+include "Inventory.php";
 header("Content-Type: application/json");
 error_reporting(0); //need no post of warnings or errors as it could changes the json output
 $apikey = "fbd4007d4eae44aebd9d387fc1a9292c"; //your api key here
 $allowed = array('hbprophecy.com', '192.168.0.2', '127.0.0.1'); //allowed domains
 $domainname = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
 
-if(!in_array($domainname, $allowed)){
+/*if(!in_array($domainname, $allowed)){
 	echo json_encode(array("results" => 'false'));
 	exit();
-}
+}*/
 if($_GET){
 	if(isset($_GET['search'])) {
 		$json = file_get_contents("https://api.spoonacular.com/recipes/search?apiKey=" . $apikey . "&query=" . urlencode($_GET['search']));
 	}
 	else if (isset($_GET['stepurl'])) {
 		$json = file_get_contents("https://api.spoonacular.com/recipes/extract?apiKey=" . $apikey . "&url=" . urlencode($_GET['stepurl']));
+	}
+	else if (isset($_GET['upc'])) {
+		//call function here to add to inventory
+		$inventory = new cInventory();
+		if($inventory->validation == true) {
+			//signed on
+			$value = $inventory->AddToInventory($_GET['upc'], $_GET['name'], $_GET['image'], $_GET['accountid']);
+			$json = json_encode(array("result"=>$value));
+		}
+		else {
+			//not a signed on user.
+			$json = json_encode(array("result"=>false));
+		}
 	}
 	else {
 		exit();
