7bd270028372532deaec69e43f1a85679a1c4e8e
Matthew Wadsworth <mdwadsworth@mail.fhsu.edu>
2021-04-12 18:05:44 -0500

    Add new Empty Activity for resturant finder.

---
+++ AndroidApp/Pantry Inventory/Pantry Inventory/FinderActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pantry_Inventory
+{
+    [Activity(Label = "FinderActivity")]
+    public class FinderActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_maps);
+            // Create your application here
+        }
+    }
+}
